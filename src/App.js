202ec42ec217317d058476a1e0c82881f522d2b9
Hari Le <hari.le@avepoint.com>
2025-02-24 22:39:16 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hari thương Mei nhiều lắm 🥰</h1>
+      <br />
+      <img style={{width: '30%'}} src={require('./mei.jpg')} />
     </div>
   );
 }
